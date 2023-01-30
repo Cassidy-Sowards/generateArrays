ec367133dcba5469d4f9cf1e9fa96df603a561f6
Cassidy Sowards <crsowards@outlook.com>
2023-01-30 18:15:22 +0100

    Update generateArrays.ts

--- generateArrays.ts
+++ generateArrays.ts
@@ -1,12 +1,15 @@
+generateArrays(4);
+
 function generateArrays(index: number){
     console.log("num is " + index);
     let output: number[][] = [];
     for(let i = 0; i < index + 1; i ++){
-        if(i <= 0){
+        if(i > 0){
           output.push([i]); 
         }
-        
     }
+
+
 
     console.log(output);
 
