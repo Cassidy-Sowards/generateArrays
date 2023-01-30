c877c77051188773df0e4587e72c0a1b1851740e
Cassidy Sowards <crsowards@outlook.com>
2023-01-30 18:08:27 +0100

    Create generateArrays.ts

---
+++ generateArrays.ts
@@ -0,0 +1,14 @@
+function generateArrays(index: number){
+    console.log("num is " + index);
+    let output: number[][] = [];
+    for(let i = 0; i < index + 1; i ++){
+        if(i <= 0){
+          output.push([i]); 
+        }
+        
+    }
+
+    console.log(output);
+
+
+}
